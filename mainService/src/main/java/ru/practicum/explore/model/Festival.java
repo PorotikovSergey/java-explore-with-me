a02874d5f5297be94187554c1be9eaf6c3c670f5
OrionChrome <sergeyporotikov@gmail.com>
2022-11-08 20:27:44 +0300

    feat: добавлен класс фестиваль

---
+++ mainService/src/main/java/ru/practicum/explore/model/Festival.java
@@ -0,0 +1,16 @@
+package ru.practicum.explore.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+//@Entity
+//@Table(name = "festivals")
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@ToString
+public class Festival {
+}
